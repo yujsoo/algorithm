249ce59fed693bb8cbcf7052b9905999481fc5ff
유지수 <jsyu333@gmail.com>
2025-02-20 02:29:33 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -0,0 +1,9 @@
+function solution(numbers) {
+    let All = 45;
+    let arrPlus = 0;
+    let result = '';
+    
+    numbers.forEach((i) => arrPlus += i)
+    
+    return result = All - arrPlus
+}
